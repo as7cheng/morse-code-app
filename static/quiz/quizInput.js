526bd8cc4444eecfe9c8845e253f2842ae65e650
zyt <243080140@qq.com>
2022-04-25 17:28:25 -0400

    quiz_validation

--- static/quiz/quizInput.js
+++ static/quiz/quizInput.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
+function validateForm() {
+    let val = $("#quizInput").val();
+    const str1 = val;
+    str1.trim();
+    for(let c of str1) {
+        console.log(c === '.');
+        if(c !== '.' && c !== '-' && c !== ' ') {
+            alert("Only '.' and '-' are allowed.");
+            $("#quizInput").focus();
+            return false;
+        }
+    }
+}
+
+$(document).ready(function () {
     // initial submit button
     $("#quizResultPrompt").empty();
     $("#quizSubmitBtn").empty();
@@ -8,22 +22,22 @@ $(document).ready(function(){
         console.log('result =', data);
 
         // if the answer is true
-        if(data && data.correctness) {
+        if (data && data.correctness) {
             $("#quizResultPrompt").empty();
             $("#quizResultPrompt").html(`
                 <p style="color: #6AA84F; font-size: 16px;">Yeah! Your answer is <b>CORRECT!</b></p>
                 <p style="color: #5E5E5E; font-size: 16px;">The right answer is <b>${data.ans}</b></p>
             `);
-            $("#quiz2Input").css('color', '#6AA84F');
-            $("#quiz2Input").attr('disabled', 'disabled');
+            $("#quizInput").css('color', '#6AA84F');
+            $("#quizInput").attr('disabled', 'disabled');
         } else { // if the answer is false
             $("#quizResultPrompt").empty();
             $("#quizResultPrompt").html(`
                 <p style="color: red; font-size: 16px;">Sorry, your answer is <b>INCORRECT.</b></p>
                 <p style="color: #5E5E5E; font-size: 16px;">The right answer is <b>${data.ans}</b></p>
             `);
-            $("#quiz2Input").css('color', 'red');
-            $("#quiz2Input").attr('disabled', 'disabled');
+            $("#quizInput").css('color', 'red');
+            $("#quizInput").attr('disabled', 'disabled');
         }
 
         $("#quizSubmitBtn").empty();
@@ -31,7 +45,7 @@ $(document).ready(function(){
 
         $("#quizScore").html(`Scores: ${data.scores}`);
 
-        $("#quizNext").click(function() {
+        $("#quizNext").click(function () {
             window.location.href = data.id < 10 ? `/quiz/${data.id + 1}` : `/quiz/final`;
         })
     });
